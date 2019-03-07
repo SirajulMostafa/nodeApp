e3a2da70b9e9f5c7f5ad60003fc427b521aeb2ee
unknown <mustafa@m.insta.kim>
2019-03-07 14:55:43 +0600

    send join table data to the indexpage view

--- controllers/indexPage.js
+++ controllers/indexPage.js
@@ -1,8 +1,11 @@
 const Post = require('../database/models/Post')
 
 module.exports =  async(req,res)  =>{
-
-    const posts = await Post.find({})
+//inside populate we give name of the terget property
+   //here we can get object of user
+   // const posts = await Post.find().populate('user_id').select('title description content createAt user_id')
+   //here we can get the target field from  user givined the second argument value and - _id we can remove _id from list
+    const posts = await Post.find().populate('user_id','username -_id').select('')
     console.log(req.session)
     console.log(posts)   
         res.render('index',{posts})//short form of Posts:Posts
